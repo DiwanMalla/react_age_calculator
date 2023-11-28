8b467d75283916c09004defb03ad79f16d9d9470
Diwan Malla <diwanmalla@Diwans-MacBook-Air.local>
2023-11-28 15:55:16 +1100

    Initial push

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Component/Header";
+import Footer from "./Component/Footer";
+import Home from "./Component/Home";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
